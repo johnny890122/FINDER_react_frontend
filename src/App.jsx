30d7a8904f03696b741ffadf134cc7eede2671ae
Lynn Chien <moneychien.aom@gmail.com>
2023-11-26 22:17:31 +0800

    feat: add tanstack query as dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Home } from './pages/Home'
 import { Tour } from './pages/Tour'
 import { GameHome } from './pages/game'
 import { NetworkSelectionPage } from './pages/game/NetworkSelection'
 
+const queryClient = new QueryClient()
+
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/tour" element={<Tour />} />
-    <Route path="/game" element={<GameHome />} />
-    <Route path="/network-selection" element={<NetworkSelectionPage />} />
-  </Routes>
+  <QueryClientProvider client={queryClient}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tour" element={<Tour />} />
+      <Route path="/game" element={<GameHome />} />
+      <Route path="/network-selection" element={<NetworkSelectionPage />} />
+    </Routes>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 )
 
 export default App
