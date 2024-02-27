087cad23ca7536b325decf730e5c9dc04dc26ddd
Lynn Chien <moneychien.aom@gmail.com>
2024-02-27 22:00:39 +0800

    feat: remove react query devtools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider as ReduxStoreProvider } from 'react-redux'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -17,7 +16,6 @@ root.render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
   </ReduxStoreProvider>,
