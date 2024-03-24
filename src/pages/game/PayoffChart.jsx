b308476aba7491e12bb17c3210009aae2ec9d1aa
Lynn Chien <moneychien.aom@gmail.com>
2024-03-24 23:11:09 +0800

    fix: payoff lines not align on xaxis

--- src/pages/game/PayoffChart.jsx
+++ src/pages/game/PayoffChart.jsx
@@ -9,13 +9,13 @@ import { parsePayoffDataForChart } from './game.utils'
 export const PayoffChart = ({ width, height }) => {
   const payoffRawData = useSelector(selectPayoff)
   const data = parsePayoffDataForChart({ payoffRawData })
-  
+
   return (
     <LineChart width={width} height={height} data={data} overflow="visible">
       <Line name="您的成績" type="monotone" dataKey="payoffHuman" stroke={color.primaryColor600} />
       <Line name="AI FINDER 的成績" type="monotone" dataKey="payoffFinder" stroke={color.neutralsColor600} />
       <CartesianGrid stroke={color.neutralsColor400} />
-      <XAxis dataKey="name" label={{ value: '回合', position: 'insideBottom', offset: -15 }} scale="band" />
+      <XAxis dataKey="name" label={{ value: '回合', position: 'insideBottom', offset: -15 }} />
       <YAxis label={{ value: '報酬', angle: -90, position: 'insideLeft', offset: 5 }} />
       <Legend verticalAlign="top" height={36} />
     </LineChart>
