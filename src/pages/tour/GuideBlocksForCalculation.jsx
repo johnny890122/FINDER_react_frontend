09c49298d46aad9d3c689c72162780fdb12c910c
lynn.chien <moneychien.aom@gmail.com>
2024-06-16 17:57:11 +0000

    feat: example 1 of tour calculation

---
+++ src/pages/tour/GuideBlocksForCalculation.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { Box, Paper, Slide } from '@mui/material'
+import { color } from '../../styles'
+import { example1StepsConfig } from './tourCalculation.config'
+
+export const GuideBlocksForCalculation = ({ step }) => (
+  <StyledSlideContainer>
+    {Object.values(example1StepsConfig).map(({ id, text }, index) => (
+      <Slide key={id} direction="up" in={step >= index} mountOnEnter unmountOnExit>
+        <StyledGuideBlock>{text}</StyledGuideBlock>
+      </Slide>
+    ))}
+  </StyledSlideContainer>
+)
+GuideBlocksForCalculation.propTypes = {
+  step: PropTypes.number.isRequired,
+}
+
+const StyledSlideContainer = styled(Box)`
+  position: relative;
+`
+const StyledGuideBlock = styled(Paper)`
+  padding: 1rem;
+  margin-top: 0.5rem;
+  background-color: ${color.primaryColor400};
+  color: white;
+`
